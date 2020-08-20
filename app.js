89193027160fe371ff57de52717b5ecc227f1a6c
nisrina <niap1502@gmail.com>
2020-08-20 16:11:59 +0700

    Commit 1

---
+++ app.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  new Vue({});
+  <head>
+    <meta charset="utf-8">
+    <title>Hello World</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue">
+    </script>
+  </head>
+  <body>
+  
+  <script src="app.js">
+
+  </script>
+  </body>
+</html>
